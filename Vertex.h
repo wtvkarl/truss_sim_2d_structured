6fb9bae7271e34af885c58857b40401c011edc43
karlscodebase <106788212+wtvkarl@users.noreply.github.com>
2024-07-24 04:06:45 -0500

    Add files via upload

---
+++ Vertex.h
@@ -0,0 +1,12 @@
+#ifndef	VERTEX_STRUCT_H
+#define VERTEX_STRUCT_H
+
+#include <GLFW/glfw3.h>
+
+struct Vertex
+{
+	GLfloat xpos, ypos, zpos;
+	GLfloat red, blue, green;
+};
+
+#endif VERTEX_STRUCT_H
